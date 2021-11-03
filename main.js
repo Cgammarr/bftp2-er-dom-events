62e1c4be0b255bcd66484515e4bddf942d1b96f2
cgammarr <carmen.gamarra@icloud.com>
2021-11-03 12:46:32 +0100

    se agregan secciones

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-    console.log("Hola!");
-})
+    let tasks = [];
+    $('#submit').on("click", function () {
+        let newTask = $('#newTask').val();
+        if (newTask !== "") {
+            tasks.push(newTask);
+
+            $('#taskList').empty()
+
+            tasks.forEach(t =>
+                $('#taskList').append(
+                    `<div class="task">${t}</div>`
+                )
+            );
+        }
+    })
+});
